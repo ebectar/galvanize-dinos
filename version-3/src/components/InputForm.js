55ff69a399283c04c3356a5273acbccf69b02529
ebectar <acebe424@gmail.com>
2018-04-11 15:33:43 -0600

    all tests passing

--- version-3/src/components/InputForm.js
+++ version-3/src/components/InputForm.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react'
 
 class InputForm extends Component {
     updatePreview() {
-        const preview = document.querySelector('#application-preview');
-        const text = document.querySelector('#application-text').value;
-        preview.textContent = text;
+        document.querySelector('#application-preview').textContent = document.querySelector('#application-text').value
       }
-      handleSubmit(event) {
-        event.preventDefault();
-        event.target.reset();
-        document.querySelector("#application-preview").textContent = "";
-        document.getElementById("message").textContent = "Your application was submitted!"
+    
+    handleSubmit(event) {
+        console.log('I was submitted!!', event)
+        event.preventDefault()
+        event.target.reset()
+        document.querySelector('#application-preview').textContent = ''
+        document.getElementById('message').textContent = 'Your application was submitted!'
       }
+
     render() {
         return (
-            <form id="application-input">
+            <form id='application-input' onSubmit={this.handleSubmit}>
                 <label>Apply Here: </label>
-                <textarea id="application-text" rows="8" cols="100" onKeyUp={this.updatePreview}></textarea>
-                <input id="submit" type="submit" value="Submit"/>
+                <textarea id='application-text' rows='8' cols='100' onKeyUp={this.updatePreview}></textarea>
+                <input id='submit' type='submit' value='Submit' />
             </form>
         )
     }
