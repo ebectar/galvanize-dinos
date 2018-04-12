0bea7d32e1474b6aaafdca883c492065a25a076f
ebectar <acebe424@gmail.com>
2018-04-12 14:16:34 -0600

    fixing deploying errors

---
+++ version-1.2/src/components/JobForm.js
@@ -0,0 +1,53 @@
+import React from 'react'
+class JobForm extends React.Component {
+    state = {
+      id: '',
+      title: '', 
+      pay: '', 
+      description: '', 
+      interested: ''
+    }
+
+    resetForm = () => {
+      this.setState(
+        {
+          id: '',
+          title: '', 
+          pay: '', 
+          description: '', 
+          interested: ''
+        }
+      )
+    }
+
+  handleInputChange = (event) => {
+    this.setState(
+      {[event.target.name]: event.target.value}
+    )
+  }
+
+  handleSubmit = (event) => {
+      event.preventDefault()
+      this.props.jobListingSubmitted(this.state)
+      this.resetForm()
+  }
+
+  render(){
+    return (
+       <aside id='side-bar'>
+          <h3>Add a Job</h3>
+          <form className='job-form' onSubmit={this.handleSubmit}>
+            <label htmlFor='title'>Title</label>
+            <input type='text' name='title' value={this.state.title} onChange={this.handleInputChange} />
+            <label htmlFor='pay'>Compensation</label>
+            <input type='text' name='pay' value={this.state.pay} onChange={this.handleInputChange} />
+            <label htmlFor='description'>Description</label>
+            <textarea name= 'description' rows='8' cols='40' value={this.state.description} onChange={this.handleInputChange}></textarea>
+            <input type='submit' name='submit' value='Submit' />
+          </form>
+        </aside>
+      )
+  }
+}
+
+export default JobForm
---
+++ version-1.2/src/components/JobList.js
@@ -0,0 +1,27 @@
+ import React from 'react'
+ 
+ class JobList extends React.Component { 
+  listItem(dinosaurs) {
+    return (
+      <li key={dinosaurs.id}>
+        <h4>{dinosaurs.title}</h4>
+        <small>{dinosaurs.pay}</small>
+        <p>{dinosaurs.description}</p>
+        <small>{dinosaurs.interested.length} dinos are interested in this job</small>
+      </li> 
+    )
+  }
+
+  render() {
+    return (
+      <section>
+        <h2>Job Listings</h2>
+        <ul id='job-listings'>
+          {this.props.listings.map(this.listItem)}
+        </ul>
+      </section>
+    )
+  }
+}
+
+ export default JobList
